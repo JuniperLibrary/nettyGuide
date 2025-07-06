393fb6a9146a41e6cbaeee3c1c366078f4700866
lin.liu <lin.liu.yd@gmail.com>
2025-07-06 17:41:18 +0800

    netty protobuf

--- src/main/java/com/uin/netty/proto/MyClientHandler.java
+++ src/main/java/com/uin/netty/proto/MyClientHandler.java
@@ -2,11 +2,14 @@ package com.uin.netty.proto;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MyClientHandler extends SimpleChannelInboundHandler<MessageProto.MyMessage> {
+
   @Override
   protected void channelRead0(ChannelHandlerContext ctx, MessageProto.MyMessage msg) {
-    System.out.println("收到服务端回复: id=" + msg.getId() + ", content=" + msg.getContent());
+    log.info("收到服务端回复: id={}, content={}", msg.getId(), msg.getContent());
   }
 
   @Override
--- src/main/java/com/uin/netty/proto/MyServerHandler.java
+++ src/main/java/com/uin/netty/proto/MyServerHandler.java
@@ -2,16 +2,18 @@ package com.uin.netty.proto;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MyServerHandler extends SimpleChannelInboundHandler<MessageProto.MyMessage> {
 
   @Override
   protected void channelRead0(ChannelHandlerContext ctx, MessageProto.MyMessage msg) {
-    System.out.println("收到客户端消息: id=" + msg.getId() + ", content=" + msg.getContent());
+    log.info("收到客户端消息: id={},content={}", msg.getId(), msg.getContent());
     // 构造响应消息
     MessageProto.MyMessage response = MessageProto.MyMessage.newBuilder()
         .setId(msg.getId() + 1000)
-        .setContent("服务端已收到: " + msg.getContent())
+        .setContent("\n服务端已收到: " + msg.getContent())
         .build();
     ctx.writeAndFlush(response);
   }
